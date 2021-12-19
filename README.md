0f4a5e4894ce3c4afe9dab3e594b9afbefa7a1a8
Jose Noriega <josenoriega723@gmail.com>
2021-12-18 21:20:19 -0700

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+# Cryptoverse
+
+> Cryptoverse is a web application to explore the cryptocurrency market.
+
+## Table of Contents
+- [Introduction](#introduction)
+- [Requirements](#requirements)
+- [Installation](#installation)
+
+## Introduction
+This application was built as a part of the course ["Build and Deploy a React Cryptocurrency App and Master Redux Toolkit in One Video"](https://www.youtube.com/watch?v=9DDX3US3kss) from the YouTube channel [JavaScript Mastery](https://www.youtube.com/channel/UCmXmlB4-HJytD7wek0Uo97A).
+
+As an extra bonus, I also added TypeScript to the application and improved the css a little bit.
+
+## Requirements
+- Node.js >= 14
+- An API Key for [Coin Ranking](https://rapidapi.com/Coinranking/api/coinranking1/)
+- An API Key for [Bing News Search](https://rapidapi.com/microsoft-azure-org-microsoft-cognitive-services/api/bing-news-search1/)
+
+## Installation
+1. Clone the repository
+  ```bash
+  $ git clone https://github.com/JoseNoriegaa/cryptoverse.git
+  ```
+2. Install dependencies
+  ```bash
+  $ npm install
+  ```
+3. Copy the `.env.example` file to `.env` and fill in the values for the API keys.
+  ```bash
+    $ cp .env.example .env
+  ```
+3. Run `npm start`
+  ```bash
+  $ npm start
+  ```
---
+++ src/App.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import {
+  Route,
+  Switch,
+  Link,
+} from 'react-router-dom';
+import {
+  Layout,
+  Typography,
+  Space,
+} from 'antd';
+
+import './App.css';
+import {
+  Navbar,
+} from './components';
+import {
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from './views';
+
+const App: React.FC = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
+    </div>
+
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Switch>
+            <Route
+              exact
+              path="/"
+            >
+              <Homepage />
+            </Route>
+
+            <Route
+              exact
+              path="/exchanges"
+            >
+              <Exchanges />
+            </Route>
+
+            <Route
+              exact
+              path="/cryptocurrencies"
+            >
+              <Cryptocurrencies />
+            </Route>
+
+            <Route
+              exact
+              path="/crypto/:coinId"
+            >
+              <CryptoDetails />
+            </Route>
+
+            <Route
+              exact
+              path="/news"
+            >
+              <News />
+            </Route>
+          </Switch>
+        </div>
+      </Layout>
+
+      <div className="footer">
+        <Typography.Title
+          level={5}
+          style={{
+            color: 'white',
+            textAlign: 'center',
+          }}
+        >
+          Cryptoverse
+          {' '}
+          <br />
+          All right reserverd.
+        </Typography.Title>
+
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
